2865211e57c0069e572573c6e79e79a1fa790c88
lucaskz <li_kaseta@hotmail.com>
2016-10-15 00:44:39 -0300

    nose

--- src/main/java/ar/edu/unlp/info/controllers/GoogleDriveOperationsController.java
+++ src/main/java/ar/edu/unlp/info/controllers/GoogleDriveOperationsController.java
@@ -1,11 +1,13 @@
 package ar.edu.unlp.info.controllers;
 
+import ar.edu.unlp.info.services.GoogleDriveAuthenticationService;
 import ar.edu.unlp.info.services.GoogleDriveOperationsService;
+
 import com.google.api.services.drive.model.File;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -19,16 +21,19 @@ import java.util.List;
 public class GoogleDriveOperationsController {
 
 	private final GoogleDriveOperationsService drive;
+	
+	private final GoogleDriveAuthenticationService helper;
 
 	@Autowired
-	public GoogleDriveOperationsController(GoogleDriveOperationsService drive) {
+	public GoogleDriveOperationsController(GoogleDriveOperationsService drive,GoogleDriveAuthenticationService helper) {
 		this.drive = drive;
+		this.helper=helper;
 	}
 
 	@RequestMapping(value = "/file", method = RequestMethod.GET)
 	public String getMyFiles(Model model) throws IOException {
-
 		List<File> files = drive.getUserFiles();
+		model.addAttribute("token", helper.getCredential().getAccessToken());
 		model.addAttribute("MyFiles", files);
 		return "files";
 
@@ -37,6 +42,7 @@ public class GoogleDriveOperationsController {
 	@RequestMapping(method = RequestMethod.GET, value = "/file/{identifier}")
 	public String getFileById(@PathVariable String identifier, Model model)	throws IOException {
 		List<File> files = drive.getFileByIdentifier(identifier);
+		model.addAttribute("token", helper.getCredential().getAccessToken());
 		model.addAttribute("MyFiles", files);
 		return "files";
 
