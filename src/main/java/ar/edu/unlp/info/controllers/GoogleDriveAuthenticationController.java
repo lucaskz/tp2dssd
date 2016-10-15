471c3fcf55156bbfe0ad73c3a7a13b33a65f27a8
EzequielPanoff <collectorsw2004@yahoo.com.ar>
2016-10-15 02:05:37 -0300

    Login solucionado...

--- src/main/java/ar/edu/unlp/info/controllers/GoogleDriveAuthenticationController.java
+++ src/main/java/ar/edu/unlp/info/controllers/GoogleDriveAuthenticationController.java
@@ -1,12 +1,15 @@
 package ar.edu.unlp.info.controllers;
 
 import ar.edu.unlp.info.services.GoogleDriveAuthenticationService;
-import ar.edu.unlp.info.services.GoogleDriveOperationsService;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -34,6 +37,25 @@ public class GoogleDriveAuthenticationController {
     public String getGoogleDriveLogin(HttpServletRequest request, HttpServletResponse response, HttpSession session, Model model) throws IOException {
 
 
+        if (helper.getCredential()!= null) {
+            String accessToken = helper.getCredential().getAccessToken();
+            RestTemplate restTemplate = new RestTemplate();
+            try {
+                String jsonResponse = restTemplate.getForObject("https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=" + accessToken, String.class);
+                JSONObject jsonObject= new JSONObject(jsonResponse);
+                try {
+                    String tokenError=jsonObject.getString("aud");
+                }
+                catch (JSONException e) {
+                    session.removeAttribute("logged");
+                }
+            }
+            catch (HttpClientErrorException e) {
+                session.removeAttribute("logged");
+            }
+
+        }
+
         if (request.getParameter("code") == null || request.getParameter("state") == null) {
 
             model.addAttribute("URL", helper.buildLoginUrl());
@@ -44,12 +66,30 @@ public class GoogleDriveAuthenticationController {
 
             try {
                 helper.saveCredentials(request.getParameter("code"));
-                model.addAttribute("logged", true);
+                session.setAttribute("logged", true);
                 return "index";
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+        return "index";
+    }
+
+    @RequestMapping (value="/revoketoken")
+    public String revokeToken (HttpSession session) {
+        if(helper.getCredential()!=null) {
+            String accessToken = helper.getCredential().getAccessToken();
+            RestTemplate restTemplate = new RestTemplate();
+            try {
+                String jsonResponse = restTemplate.getForObject("https://accounts.google.com/o/oauth2/revoke?token=" + accessToken, String.class);
+                session.removeAttribute("logged");
+            } catch (HttpClientErrorException e) {
+
+            }
+
+            return "redirect:/index";
+        }
+        session.removeAttribute("logged");
         return "index";
     }
 }
