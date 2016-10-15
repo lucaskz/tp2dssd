47fd9729833a56949acd4c9090c4efcbffe53c46
lucaskz <li_kaseta@hotmail.com>
2016-10-14 22:45:40 -0300

    listado de archivso

--- src/main/java/ar/edu/unlp/info/services/GoogleDriveOperationsService.java
+++ src/main/java/ar/edu/unlp/info/services/GoogleDriveOperationsService.java
@@ -3,7 +3,9 @@ package ar.edu.unlp.info.services;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.model.File;
 import com.google.api.services.drive.model.FileList;
+
 import org.springframework.stereotype.Service;
+
 
 
 import java.io.IOException;
@@ -26,14 +28,37 @@ public class GoogleDriveOperationsService {
         FileList request = service.files().list()
                 .setPageSize(30)
                 .setQ("'root' in parents")
-                .setFields("nextPageToken, files(id, name)")
+                .setFields("nextPageToken, files(id, name,modifiedTime)")
+                .execute();
+        
+        return request.getFiles();
+
+    }
+    
+    public List<File> getFiles(Integer id) throws IOException {
+        // Build a new authorized API client service.
+        Drive service = getDriveService();
+        FileList request = service.files().list()
+                .setPageSize(30)
+                .setQ(id + " in parents")
+                .setFields("nextPageToken, files(id, name,modifiedTime)")
                 .execute();
         
         return request.getFiles();
 
     }
 
-
+	public List<File> getFileByIdentifier(String identifier) throws IOException {
+        // Build a new authorized API client service.
+        Drive service = getDriveService();
+        FileList request = service.files().list()
+                .setPageSize(30)
+                .setQ(identifier + " in parents")
+                .setFields("nextPageToken, files(id, name,modifiedTime)")
+                .execute();
+        
+        return request.getFiles();
+	}
 
 }
 
