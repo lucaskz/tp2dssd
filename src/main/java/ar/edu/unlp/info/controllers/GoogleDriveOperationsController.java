42b43087b12978f842f5b8599e49236780395e51
Matias <ma77i_lp_90@hotmail.com>
2016-10-16 05:15:46 -0300

    CREAR DOCUMENTO FUNCIONANDO , FALTA DETERMINAR CUANDO UN ELEMENTO ES UN DIRECTORIO O ARCHIVO, DEPENDIENDO DE ESTO SE MUESTRO EL ICONO CORRESPONDIENTE, , EN LOS METODOS DE LA API NO ENCONTRE UNO QUE DETERMINE ESO, A SEGUIR BUSCANDO xD

--- src/main/java/ar/edu/unlp/info/controllers/GoogleDriveOperationsController.java
+++ src/main/java/ar/edu/unlp/info/controllers/GoogleDriveOperationsController.java
@@ -1,12 +1,15 @@
 package ar.edu.unlp.info.controllers;
 
+import ar.edu.unlp.info.services.GoogleDriveAuthenticationService;
 import ar.edu.unlp.info.services.GoogleDriveOperationsService;
 import com.google.api.services.drive.model.File;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.IOException;
 import java.util.List;
@@ -18,28 +21,43 @@ import java.util.List;
 @Controller
 public class GoogleDriveOperationsController {
 
-	private final GoogleDriveOperationsService drive;
+    private final GoogleDriveOperationsService drive;
 
-	@Autowired
-	public GoogleDriveOperationsController(GoogleDriveOperationsService drive) {
-		this.drive = drive;
-	}
+    private final GoogleDriveAuthenticationService helper;
 
-	@RequestMapping(value = "/file", method = RequestMethod.GET)
-	public String getMyFiles(Model model) throws IOException {
+    @Autowired
+    public GoogleDriveOperationsController(GoogleDriveOperationsService drive, GoogleDriveAuthenticationService helper) {
+        this.drive = drive;
+        this.helper = helper;
+    }
 
-		List<File> files = drive.getUserFiles();
-		model.addAttribute("MyFiles", files);
-		return "files";
-
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/file/{identifier}")
-	public String getFileById(@PathVariable String identifier, Model model)	throws IOException {
-		List<File> files = drive.getFileByIdentifier(identifier);
+    @RequestMapping(value = "/file", method = RequestMethod.GET)
+    public String getMyFiles(Model model) throws IOException {
+        List<File> files = drive.getUserFiles();
+        model.addAttribute("token", helper.getCredential().getAccessToken());
         model.addAttribute("MyFiles", files);
-		return "files";
+        return "files";
 
-	}
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/file/{identifier}")
+    public String getFileById(@PathVariable String identifier, Model model) throws IOException {
+        List<File> files = drive.getFileByIdentifier(identifier);
+
+        model.addAttribute("MyFiles", files);
+        model.addAttribute("token", helper.getCredential().getAccessToken());
+        model.addAttribute("MyFiles", files);
+        return "files";
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/file/create")
+    public String createDocument(Model model, HttpServletRequest request) throws IOException {
+
+        drive.createDocument(request.getParameter("nombre"));
+        model.addAttribute("token", helper.getCredential().getAccessToken());
+        model.addAttribute("MyFiles", drive.getUserFiles());
+        return "files";
+    }
 
 }
